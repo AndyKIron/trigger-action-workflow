24576cd03f55b56dd97cab008b6e4876253fb546
AndyKIron <andyk@is.com>
2021-11-28 16:49:50 +0200

    ci:  changes in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -71,12 +71,13 @@ validate_args() {
 trigger_workflow() {
   echo "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches"
 
-  curl --fail -X POST "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches" \
+  trigger_workflow=$(curl --fail -X POST "${GITHUB_API_URL}/repos/${INPUT_OWNER}/${INPUT_REPO}/actions/workflows/${INPUT_WORKFLOW_FILE_NAME}/dispatches" \
     -H "Accept: application/vnd.github.v3+json" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" \
-    --data "{\"ref\":\"${ref}\",\"inputs\":${inputs}}"
+    --data "{\"ref\":\"${ref}\",\"inputs\":${inputs}}")
 
+  echo "Trigger response ${trigger_workflow}"
   echo "Sleeping for ${wait_interval} seconds"
   sleep $wait_interval
 }
