24e0aff47f92a67af1d08648949e568bc3812f45
AndyKIron <andyk@is.com>
2021-11-30 11:51:01 +0200

    ci:  changes in script

--- entrypoint.sh
+++ entrypoint.sh
@@ -166,7 +166,7 @@ wait_for_workflow_to_finish() {
 main() {
   validate_args
 
-  echo ">>>> $INPUT_JOB_NAME_SUBSRING"
+  echo ">>>> ${INPUT_JOB_NAME_SUBSTRING} :: ${INPUT_OWNER}"
 
 #  if [ "${trigger_workflow}" = true ]
 #  then
